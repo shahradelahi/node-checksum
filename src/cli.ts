1f3828bf1511127a80d74efd7ec32c86985c2a05
Shahrad Elahi <shahradq@gmail.com>
2024-04-26 22:55:58 +0330

    chore: Bump to v0.2.1 (#8)

--- src/cli.ts
+++ src/cli.ts
@@ -12,7 +12,7 @@ Checksum utility CLI
 Author: @shahradelahi, https://github.com/shahradelahi
 `
   )
-  .version('0.2.0')
+  .version('0.2.1')
   .addCommand(verifyCmd)
   .addCommand(hashCmd)
   .parse(process.argv)
