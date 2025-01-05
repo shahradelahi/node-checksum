82c1478fb55da82867b24de1beb28db7af1654ef
Shahrad Elahi <shahrad@litehex.com>
2025-01-05 12:35:42 +0000

    fix: `eslint` config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,12 @@
 import { defineConfig } from '@shahrad/eslint-config';
 
-export default defineConfig({
-  ignores: ['dist'],
-});
+export default defineConfig(
+  { ignores: ['dist'] },
+  {
+    rules: {
+      'no-console': 'error',
+      '@typescript-eslint/semi': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  }
+);
